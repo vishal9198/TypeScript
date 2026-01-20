016a77e8934df9aefc5808d451c110de9739ba84
vishal9198 <vkgupta4558@gmail.com>
2026-01-20 17:13:27 +0530

    learning Enum

--- node-app/src/a.ts
+++ node-app/src/a.ts
@@ -1,10 +1,19 @@
-interface manager {
-  name: string;
+// type keyword = "up" | "down" | "left" | "right"; //hard coded  const value
+//enum is used to combined const  values
+
+enum Direction {
+  Left = "left",
+  Right = "right",
+  Up = "up",
+  Down = "down",
 }
 
-interface User {
-  age: string;
+function dosomthing(keyPressed: Direction) {
+  if (keyPressed == Direction.Up) console.log("up");
 }
-interface bigperson extends manager, User {
-  mob: number;
-}
+
+dosomthing(Direction.Up);
+dosomthing(Direction.Down);
+console.log(Direction.Up);
+console.log(Direction.Down);
+// dosomthing("leftword");
